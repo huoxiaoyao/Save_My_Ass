e6c1378d4df9a8242759ca50f08613a79ff830c8
SimonRingeisen <simon.ringeisen@gmail.com>
2015-12-17 16:20:08 +0100

    added colors and removed static strings in intro

--- SaveMyAss/app/src/main/java/ch/ethz/inf/vs/a4/savemyass/UI/Intro.java
+++ SaveMyAss/app/src/main/java/ch/ethz/inf/vs/a4/savemyass/UI/Intro.java
@@ -12,20 +12,14 @@ public class Intro extends AppIntro {
 
     @Override
     public void init(Bundle savedInstanceState) {
-        //TODO: set some nice colors
-        // Instead of fragments, you can also use our default slide
-        // Just set a title, description, background and image. AppIntro will do the rest
-        addSlide(AppIntroFragment.newInstance("Welcome to SaveMyAss", "", R.mipmap.ic_launcher, R.color.colorAccent));
-        addSlide(AppIntroFragment.newInstance("Geo-Location", "We track your location with GPS. an active internet connection is required.", R.raw.geolocation, R.color.colorAccent));
-        addSlide(AppIntroFragment.newInstance("Peer-to-Peer", "In case of an emergency, you can connect to devices near to you.", R.raw.p2p, R.color.colorAccent));
-        addSlide(AppIntroFragment.newInstance("Security", "Your location is stored secure and hidden from others.", R.raw.secure, R.color.colorAccent));
+        addSlide(AppIntroFragment.newInstance(getResources().getString(R.string.intro_welcome), getResources().getString(R.string.intro_welcome_text), R.raw.logohelp, getResources().getColor(R.color.accent)));
+        addSlide(AppIntroFragment.newInstance(getResources().getString(R.string.intro_location), getResources().getString(R.string.intro_location_text), R.raw.geolocation, getResources().getColor(R.color.accent)));
+        addSlide(AppIntroFragment.newInstance(getResources().getString(R.string.intro_p2p), getResources().getString(R.string.intro_p2p_text), R.raw.p2p, getResources().getColor(R.color.accent)));
+        addSlide(AppIntroFragment.newInstance(getResources().getString(R.string.intro_security), getResources().getString(R.string.intro_security_text), R.raw.secure, getResources().getColor(R.color.accent)));
 
-        // OPTIONAL METHODS
-        // Override bar/separator color
-        setBarColor(Color.parseColor("#1A237E"));
-        setSeparatorColor(Color.parseColor("#1A237E"));
+        setBarColor(getResources().getColor(R.color.primary_dark));
+        setSeparatorColor(getResources().getColor(R.color.divider));
 
-        // Hide Skip/Done button
         showSkipButton(true);
         showDoneButton(true);
 
@@ -33,7 +27,6 @@ public class Intro extends AppIntro {
 
     @Override
     public void onSkipPressed() {
-        // Do something when users tap on Skip button.
         finish();
     }
 
@@ -44,7 +37,6 @@ public class Intro extends AppIntro {
 
     @Override
     public void onDonePressed() {
-        // Do something when users tap on Done button.
         finish();
     }
 
